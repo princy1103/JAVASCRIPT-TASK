5fe8230f156c9c6cf31214061627460841b2877c
princy1103 <princyjoshi5670@gmail.com>
2025-04-11 15:42:52 +0530

    Json

--- API/Weather/script.js
+++ API/Weather/script.js
@@ -1,8 +1,9 @@
 //https://api.openweathermap.org/data/2.5/weather?q=surat&appid=8d9f7c9674d1ea67b496323e419a1fd3&units=metric
 const uimaker = (data) => {
+    let roundTemp=Math.round(data.main.temp);
     let temp = `
         <p class="location"><i class="fa-solid fa-location-crosshairs"></i> ${data.name}</p>
-        <p class="temperature"><i class="fa-solid fa-cloud-sun-rain"></i> ${data.main.temp}°C</p>
+        <p class="temperature"><i class="fa-solid fa-cloud-sun-rain"></i> ${roundTemp}°C</p>
         <div class="info">
             <div>
                 <h3><i class="fa-solid fa-wind"></i> Wind</h3>
@@ -29,16 +30,32 @@ const uimaker = (data) => {
         `
     document.getElementById("output").innerHTML = temp;
 };
-const getWeather = async (location) => {
-    let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=8d9f7c9674d1ea67b496323e419a1fd3&units=metric`);
+const getWeather = async (cityname) => {
+    let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=8d9f7c9674d1ea67b496323e419a1fd3&units=metric`);
     let res = await req.json();
-    console.log(res);
+    //console.log(res);
     uimaker(res);
 };
+//weather by location 
+const getWeatherBylocation = async (lat,long) => {
+    let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=8d9f7c9674d1ea67b496323e419a1fd3&units=metric`);
+    let res = await req.json();
+    //console.log(res);
+    uimaker(res);
+};
+const getlocation=()=>{
+    navigator.geolocation.getCurrentPosition((pos)=>{
+        console.log(pos.coords)
+        const lat=pos.coords.latitude;
+        const long=pos.coords.longitude;
+        getWeatherBylocation(lat,long)
+    }); 
+};
+getlocation();
 document.getElementById("search").addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
-        let location = e.target.value;
-        console.log(location);
-        getWeather(location);
+        let cityname = e.target.value;
+        console.log(cityname);
+        getWeather(cityname);
     }
 });
